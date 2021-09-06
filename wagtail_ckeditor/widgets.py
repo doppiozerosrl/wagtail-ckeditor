5775a339ea5478c6349f84c40dec7c6cab973c20
Caio Jhonny <caiojhonnyp@gmail.com>
2021-09-06 13:04:23 +0200

    Compatibility adjustments

--- wagtail_ckeditor/widgets.py
+++ wagtail_ckeditor/widgets.py
@@ -7,14 +7,36 @@ import json
 from django.forms import widgets
 from django.utils.safestring import mark_safe
 from wagtail.utils.widgets import WidgetWithScript
-from wagtail.admin.edit_handlers import RichTextFieldPanel
-from wagtail.core.rich_text import DbWhitelister
-from wagtail.core.rich_text import expand_db_html
+
+from wagtail import __version__ as WAGTAIL_VERSION
+
+if WAGTAIL_VERSION >= '2.0':
+    from wagtail.admin.edit_handlers import RichTextFieldPanel
+    from wagtail.admin.rich_text.converters.editor_html import EditorHTMLConverter
+    from wagtail.core.rich_text import features
+else:
+    from wagtail.wagtailadmin.edit_handlers import RichTextFieldPanel
+    from wagtail.wagtailcore.rich_text import DbWhitelister
+    from wagtail.wagtailcore.rich_text import expand_db_html
 
 from wagtail_ckeditor import settings
 
 
 class CKEditor(WidgetWithScript, widgets.Textarea):
+
+    def __init__(self, attrs=None, **kwargs):
+        super(CKEditor, self).__init__(attrs)
+        self.kwargs = self.getDefaultArgs()
+        self.features = kwargs.pop('features', None)
+        if kwargs is not None:
+            self.kwargs.update(kwargs)
+
+        if WAGTAIL_VERSION >= '2.0':
+            if self.features is None:
+                self.features = features.get_default_features()
+                self.converter = EditorHTMLConverter()
+            else:
+                self.converter = EditorHTMLConverter(self.features)
 
     def get_panel(self):
         return RichTextFieldPanel
@@ -23,7 +45,10 @@ class CKEditor(WidgetWithScript, widgets.Textarea):
         if value is None:
             translated_value = None
         else:
-            translated_value = expand_db_html(value, for_editor=True)
+            if WAGTAIL_VERSION >= '2.0':
+                translated_value = self.converter.from_database_format(value)
+            else:
+                translated_value = expand_db_html(value, for_editor=True)
         return super().render(name, translated_value, attrs)
 
     def render_js_init(self, editor_id, name, value):
@@ -34,4 +59,8 @@ class CKEditor(WidgetWithScript, widgets.Textarea):
         original_value = super().value_from_datadict(data, files, name)
         if original_value is None:
             return None
-        return DbWhitelister.clean(original_value)
+        if WAGTAIL_VERSION >= '2.0':
+            return self.converter.to_database_format(original_value)
+        else:
+            return DbWhitelister.clean(original_value)
+
